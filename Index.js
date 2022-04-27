052d295c8f8e70e753930a8a279c4ddd72c15567
Luis Isaza <luisisaza964@yahoo.com>
2022-04-26 19:33:03 -0500

    Tarea

--- Index.js
+++ Index.js
@@ -1,25 +1,47 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+let id, Nombre1 , Nombre2 ;
+let Edad;
 
-app.get('/spanish', function (req, res) {
+
+
+app.use(express.json());
+
+app.get('/', function (req, res) {
   res.send('Hola Mundo')
 })
 
-app.get('/frances', function (req, res) {
-  res.send('Salut Monde')
+app.get('/id', function (req, res) {
+  res.send(id)
+  console.log(id); 
 })
 
-app.get('/griego', function (req, res) {
-  res.send('Ciao mondo')
+app.get('/name', function (req, res) {
+  res.send(Nombre1)
+  console.log(Nombre1); 
 })
 
-app.get('/japones', function (req, res) {
-  res.send('こんにちは世界')
+app.get('/lastName', function (req, res) {
+  res.send(Nombre2)
+  console.log(Nombre2); 
 })
+
+app.get('/old', function (req, res) {
+  res.send(Edad);
+  console.log(Edad); 
+})
+
+app.post('/', function(request, response){
+  
+  id =  request.body.id
+  Nombre1 =  request.body.name
+  Nombre2 =  request.body.lastName
+  Edad =  request.body.old
+  console.log(request.body);      // your JSON
+
+  response.send("Exitoso");    // echo the result back
+});
 
 
 
